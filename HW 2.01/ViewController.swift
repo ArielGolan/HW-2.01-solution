f185d5b6337f58c9f22c2c004cf23f96381c03b3
Ariel Golan <ariel.golan.ag@gmail.com>
2022-06-15 10:45:53 +0300

    Initial Commit

---
+++ HW 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW 2.01
+//
+//  Created by Ariel Golan on 15.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
